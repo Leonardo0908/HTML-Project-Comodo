eb33a2ade1f84b54242e6ddbc036b55925bf486f
Leonardo E S Rodrigues <leonardo.elias0908@gmail.com>
2022-05-07 13:27:33 -0300

    adicionando funcionalidades

---
+++ README.md
@@ -0,0 +1,64 @@
+<h1 align="center" style="color: blue" > Projeto Comodo </h1>
+
+<h2 align="center" style="color: #00BFFF " > ...passo a passo de como usar a aplicação... </h2>
+
+# Instalando
+<h3> Para acessar a aplicação click no link a baixo </h3>
+
+<h3 style="color: #C0C0C0 " > 1° acesse esse link a baixo</h3>
+ <a> https://gitlab.com/Leonardo0908/projetocomodo01 </a> 
+ <p> click em Download e depois baixe o arquivo em zip </p>
+
+<img src="./assets/img/img1.png" alt="Imagem 1"><br><br><br>
+
+<h3 style="color: #C0C0C0 " > 2° extrair zip </h3>
+
+<img src="./assets/img/img2.png" alt="Imagem 1"><br><br><br>
+
+<h3 style="color: #C0C0C0 " > 3° Entre na pasta já estraida e acesse o aquivo index </h3><br><br>
+<p> Após abrir o index é só usar a aplicação </p>
+
+<img src="./assets/img/animação1.gif" alt="Imagem 1"><br><br><br>
+
+# Como Funciona
+
+<h3 style="color: #C0C0C0 " > 1° Preencha os campos </h3>
+
+<img src="./assets/img/img01.png" alt="Imagem 1"><br><br><br>
+
+<h3 style="color: #C0C0C0 " > 2°  Leia e aperte o botão calcular  </h3>
+
+<img src="./assets/img/img02.png" alt="Imagem 1"><br><br><br>
+
+<h3 style="color: #C0C0C0 " > 3° Podem ocorrer erros  </h3>
+
+<p> Fique tranquilo sempre que ouver um erro ele emitira um alerta explicando o possivel erro </p>
+<p> Click em OK e tente novamente, não esqueca de ler os alertas </p>
+
+<img src="./assets/img/img03.png" alt="Imagem 1"><br><br><br>
+
+<h3 style="color: #C0C0C0 " > 4° Os Erros acrecentaram o valor 0 a parede </h3>
+
+<p> Fique tranquilo sempre que ouver um erro você pode tentar novamente sem perder o que já foi feito </p>
+
+<img src="./assets/img/img04.png" alt="Imagem 1"><br><br><br>
+
+<h3 style="color: #C0C0C0 " > 5° Limpando Campos </h3>
+
+<p> Se você digitou algo errado e quer começar novamente basta apertar o botão limpar </p>
+<p> Se quiser fazer uma nova conta aperte o botão limpar </p>
+
+<img src="./assets/img/img05.png" alt="Imagem 1"><br><br><br>
+
+<h3 style="color: #C0C0C0 " > 6° Resultados </h3>
+
+<p> Destacados em vermelho os campos que seram apresentados os resultados do valor final </p>
+<p> A cada cálculo o valor é atualizado assim o você pode ver cada passo </p>
+
+<img src="./assets/img/img06.png" alt="Imagem 1"><br><br><br>
+
+
+
+# Link da Aplicação na Nuvem
+
+## ⚠️ !!EM Desenvolvimento!! ⚠️ 
---
+++ assets/js/script.js
@@ -0,0 +1,183 @@
+
+const porta = 1.52
+const janela = 2.4
+
+let por = false
+let jan = false
+let larOk = false
+let altOk = false
+let opcOk = false
+
+let lataGG = 0
+let lataG = 0
+let lataM = 0
+let lataP = 0
+
+let parede = [4]
+let i = 0
+
+let final = 0
+
+function calculaParede(){
+    do{
+    
+    let largura = document.querySelector(".largura").value.replace(',' , '.')
+    let altura = document.querySelector(".altura").value.replace(',' , '.')
+
+    parede[i] = parseFloat(largura) * parseFloat(altura)
+
+    do{
+        
+      let opcao = document.querySelector(".opcao").value
+
+        switch(parseInt(opcao)){
+            case 0:
+                por = false
+                jan = false
+                break;
+            case 1:
+                parede[i] -= porta + janela   
+                por = true
+                jan = true
+                break;
+            case 2:
+                parede[i] -= porta
+                por = true
+                break;
+            case 3:
+                parede[i] -= janela
+                jan = true
+                break
+            default:
+                parede[i] = "undefined"
+                break
+        }
+    
+    }while(larOk == false && altOk == false && opcOk == false)
+        
+        if(por == true && altura < 2.20){
+            alert("A altura de paredes com porta deve ser, no mínimo, 30 centímetros maior que a altura da porta");
+            por = false 
+            jan = false 
+            parede[i] = 0;
+            break  
+        }
+        else if(opcOk == false || altOk == false || larOk == false){
+            alert(" Ocorreu Um Erro algo esta inválido... ")
+            parede[i] = 0
+            por = false 
+            jan = false 
+            break
+        }
+        else if(parede[i] < 1){
+            alert("Parede com menos de um metro... ")
+            parede[i] = 0
+            por = false 
+            jan = false 
+            break
+        }
+        else if(largura == 0 || altura == 0) {
+            alert("valor errado")
+            parede[i] = 0
+            por = false 
+            jan = false 
+            break
+        }
+        
+        else if( parede[i] > 15){
+            alert(" A parede não pode ter mais que 15 Metros Quadrado ")
+            parede[i] = 0
+            por = false 
+            jan = false 
+            break
+        }
+        else if(jan == true && por == true && parede[i] / 2 < porta * janela){
+            alert(" porta e janela deve ser no maximo 50% da area da parede")
+            parede[i] = 0
+            por = false 
+            jan = false 
+            break 
+        }
+        else if(por == false && jan == true && parede[i] / 2 < janela){
+            alert(" janela deve ser no maximo 50% da area da parede")
+            parede[i] = 0
+            por = false 
+            jan = false 
+            break
+        }
+        else if( jan == false && por == true && parede[i] / 2 < porta){
+            alert(" porta deve ser no maximo 50% da area da parede")
+            parede[i] = 0
+            por = false 
+            jan = false 
+            break
+        }
+        
+        else if(i < 4){
+            final +=  parede[i]
+            jan = false
+            por = false
+        }
+               
+        }while(parede[i] == 0)
+        
+        if(parede[i] != 0){
+            i++
+    }
+        document.querySelector(".parede1").innerHTML = parede[0].toFixed(2)
+        document.querySelector(".parede2").innerHTML = parede[1].toFixed(2)
+        document.querySelector(".parede3").innerHTML = parede[2].toFixed(2)
+        document.querySelector(".parede4").innerHTML = parede[3].toFixed(2)
+        document.querySelector(".final").innerHTML = final.toFixed(2)
+
+        lataGG = final / 90
+        lataG = (final % 90) / 18
+        lataM = ((final % 90) % 18) / 12.5
+        lataP =  (((final % 90) % 18) % 12.5) % 2.5
+
+        
+
+        document.querySelector(".lataGG").innerHTML = parseInt(lataGG)
+        document.querySelector(".lataG").innerHTML = parseInt(lataG)
+        document.querySelector(".lataM").innerHTML = parseInt(lataM)
+        document.querySelector(".lataP").innerHTML = parseInt(lataP)    
+}
+
+
+function validaLargura(){
+    let txtLargura = document.querySelector("#txtLargura")
+    if(largura.value <= 0 ){
+        txtLargura.innerHTML = "Valor inválido"
+        txtLargura.style.color = "red"
+         larOk = false
+    }else{
+        txtLargura.innerHTML = "Valor Valido"
+        txtLargura.style.color = "green"
+         larOk = true
+    }
+}
+function validaAltura(){
+    let txtAltura = document.querySelector("#txtAltura")
+    if(altura.value <= 0 ){
+        txtAltura.innerHTML = "Valor inválido"
+        txtAltura.style.color = "red"
+         altOk = false
+    }else{
+        txtAltura.innerHTML = "Valor Valido"
+        txtAltura.style.color = "green"
+        altOk = true 
+    }
+}
+function validaOpcao(){
+    let txtOpcao = document.querySelector("#txtOpcao")
+    if( opcao.value.length < 1 || opcao.value >= 4 || opcao.value < 0  ){
+        txtOpcao.innerHTML = "Valor inválido"
+        txtOpcao.style.color = "red"
+        opcOk = false
+    }else if(opcao.value == 0 || opcao.value == 1 || opcao.value == 2 || opcao.value == 3) {
+        txtOpcao.innerHTML = "Valor Valido"
+        txtOpcao.style.color = "green"
+        opcOk = true
+    }
+}
+
